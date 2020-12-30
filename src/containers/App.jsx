852b446670c26a8b4ca25d60050d40c1e50d4a18
lrojassc <liever.rojas@sdevelopers.co>
2020-12-29 19:44:57 -0500

    Finalizando curso

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,44 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
+
+const API = 'http://localhost:3000/initalState';
 
 import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () => {
+    const [videos, categories] = useInitialState(API);
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+            {categories.map((category) => (
+                videos[category].length > 0 && (
+                    <Categories title={category}>
+                        <Carousel>
+                            {videos[category].map((item) => (<CarouselItem key={item.id} {...item} />))}
+                        </Carousel>
+                    </Categories>
+                )))}
 
-        <Categories title="Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-
-        <Categories title="Originales">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-
-        <Footer />
-    </div>
-);
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+const useInitialState = (API) => {
+    const [videos, setVideos ] = useState({ "mylist": [], "trends": [], "originals": [] });
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch(API)
+            .then(response => response.json())
+            .then(data => {
+                setVideos(data);
+                setCategories(Object.keys(data));
+            });
+    }, []);
+    return [videos, categories];
+}
+
+export default useInitialState;
